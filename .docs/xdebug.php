2f2c67a851476a0678cda33bd397d574b9a2d4e4
Jhorman Alexander Tasayco <jhormantasayco@gmail.com>
2020-03-19 18:32:09 -0500

    [Add] Licence Mit

--- .docs/xdebug.php
+++ .docs/xdebug.php
@@ -8,6 +8,10 @@
  * @link (https://gist.github.com/odan/1abe76d373a9cbb15bed)
  * @link (https://medium.com/d6-digital/installing-xdebug-for-xampp-with-php-in-windows-d2b750861118)
  *
+ **** Enlaces ****
+ *
  * https://semver.org/
+ * https://choosealicense.com/
+ * https://tldrlegal.com/
  *
  */
